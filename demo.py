b3655e528f121c1b422cb981b533c8b9cd3dbae5
yatendra054 <yatendrapachori54@gmail.com>
2026-06-23 12:59:17 +0530

    Add Files

---
+++ demo.py
@@ -0,0 +1,4 @@
+from src.logger import logging
+
+logging.warning("This is a warning message.")
+logging.error("This is an error message.")
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Yatendra",
+    author_email="yatendrapachroi54@gmail.com",
+    packages=find_packages()
+)
