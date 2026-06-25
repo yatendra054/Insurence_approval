172063285097c8396da357f9a0b0873338233e67
yatendra054 <yatendrapachori54@gmail.com>
2026-06-25 17:43:59 +0530

    Add Files

--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -5,3 +5,16 @@ from dataclasses import dataclass
 class DataIngestionArtifact:
     trained_file_path:str 
     test_file_path:str
+ 
+@dataclass
+class DataValidationArtifact:
+    validation_status:bool
+    message: str
+    validation_report_file_path: str
+    
+    
+@dataclass
+class DataTransformationArtifact:
+    transformed_object_file_path:str 
+    transformed_train_file_path:str
+    transformed_test_file_path:str        
