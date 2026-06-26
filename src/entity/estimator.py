2d39e2f791602ac0c9562a0b5ee789fb2bbc7641
yatendra054 <yatendrapachori54@gmail.com>
2026-06-26 12:06:50 +0530

    model_train

---
+++ src/entity/estimator.py
@@ -0,0 +1,47 @@
+import sys
+
+import pandas as pd
+from pandas import DataFrame
+from sklearn.pipeline import Pipeline
+
+from src.exception import MyException
+from src.logger import logging
+
+class TargetValueMapping:
+    def __init__(self):
+        self.yes:int = 0
+        self.no:int = 1
+    def _asdict(self):
+        return self.__dict__
+    def reverse_mapping(self):
+        mapping_response = self._asdict()
+        return dict(zip(mapping_response.values(),mapping_response.keys()))
+
+class MyModel:
+    def __init__(self, preprocessing_object: Pipeline, trained_model_object: object):
+        self.preprocessing_object = preprocessing_object
+        self.trained_model_object = trained_model_object
+
+    def predict(self, dataframe: pd.DataFrame) -> DataFrame:
+        try:
+            logging.info("Starting prediction process.")
+
+            # Step 1: Apply scaling transformations using the pre-trained preprocessing object
+            transformed_feature = self.preprocessing_object.transform(dataframe)
+
+            # Step 2: Perform prediction using the trained model
+            logging.info("Using the trained model to get predictions")
+            predictions = self.trained_model_object.predict(transformed_feature)
+
+            return predictions
+
+        except Exception as e:
+            logging.error("Error occurred in predict method", exc_info=True)
+            raise MyException(e, sys) from e
+
+
+    def __repr__(self):
+        return f"{type(self.trained_model_object).__name__}()"
+
+    def __str__(self):
+        return f"{type(self.trained_model_object).__name__}()"
--- src/pipline/training_pipeline.py
+++ src/pipline/training_pipeline.py
@@ -5,10 +5,11 @@ from src.logger import logging
 from src.components.data_ingestion import DataIngestion
 from src.components.data_validation import DataValidation
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
-from src.entity.config_entity import (DataIngestionConfig,DataValidationConfig,DataTransformationConfig)
+from src.entity.config_entity import (DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig)
                                           
-from src.entity.artifact_entity import (DataIngestionArtifact,DataValidationArtifact,DataTransformationArtifact)
+from src.entity.artifact_entity import (DataIngestionArtifact,DataValidationArtifact,DataTransformationArtifact,ModelTrainerArtifact)
 
 
 
@@ -17,6 +18,7 @@ class TrainPipeline:
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config=DataValidationConfig()
         self.data_transformation_config=DataTransformationConfig()
+        self.model_trainer_config=ModelTrainerConfig()
 
 
     
@@ -56,7 +58,17 @@ class TrainPipeline:
             data_transformation_artifact = data_transformation.initiate_data_transformation()
             return data_transformation_artifact
         except Exception as e:
-            raise MyException(e, sys)      
+            raise MyException(e, sys)
+    
+    def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
+        try:
+            model_trainer = ModelTrainer(data_transformation_artifact=data_transformation_artifact,
+                                         model_trainer_config=self.model_trainer_config)
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise MyException(e, sys)     
         
 
     def run_pipeline(self, ) -> None:
@@ -64,7 +76,10 @@ class TrainPipeline:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
             data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
-            self.data_transformation_artifact=self.start_data_transformation(data_ingestion_artifact=data_ingestion_artifact,data_validation_artifact=data_validation_artifact)
+            data_transformation_artifact=self.start_data_transformation(data_ingestion_artifact=data_ingestion_artifact,data_validation_artifact=data_validation_artifact)
+            model_trainer_artifact=self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
             
         except Exception as e:
             raise MyException(e, sys)
+        
+        
